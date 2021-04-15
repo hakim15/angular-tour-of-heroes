a631e7a896ea2dd34cf2829fc2e91c29050b88b3
hakim15 <37046419+hakim15@users.noreply.github.com>
2021-04-15 19:12:38 -0400

    g

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MesssagesComponent } from './messsages/messsages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyCompComponent } from './fold/my-comp/my-comp.component';
+import { MonCompComponent } from './mon-comp/mon-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MesssagesComponent,
+    DashboardComponent,
+    MyCompComponent,
+    MonCompComponent
   ],
   imports: [
     BrowserModule,
