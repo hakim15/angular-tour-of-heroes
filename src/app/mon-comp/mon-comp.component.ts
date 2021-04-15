ab4033a45b7123217f1785117eb5256a5acc579d
hakim15 <37046419+hakim15@users.noreply.github.com>
2021-04-15 19:46:57 -0400

    fin

---
+++ src/app/mon-comp/mon-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-comp',
+  templateUrl: './mon-comp.component.html',
+  styles: []
+})
+export class MonCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
